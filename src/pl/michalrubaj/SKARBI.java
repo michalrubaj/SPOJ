7f98fad79bebac4154c4841a0a9d50110ab1f4db
MichalRubaj <michalrubaj117@gmail.com>
2018-09-18 00:46:16 +0200

    refactored code

--- src/pl/michalrubaj/SKARBI.java
+++ src/pl/michalrubaj/SKARBI.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class SKARBI {
@@ -12,12 +11,13 @@ public class SKARBI {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int howManyTimes = Integer.parseInt(bufferedReader.readLine());
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 0);
-        map.put(1, 0);
-        map.put(2, 0);
-        map.put(3, 0);
+
         for (int i = 0; i < howManyTimes; i++) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(0, 0);
+            map.put(1, 0);
+            map.put(2, 0);
+            map.put(3, 0);
             int howManySteps = Integer.parseInt(bufferedReader.readLine());
             for (int j = 0; j < howManySteps; j++) {
                 String[] clues = bufferedReader.readLine().split(" ");
@@ -27,28 +27,22 @@ public class SKARBI {
                     map.put(direction, map.get(direction) + stepsInThatDirection);
                 }
             }
-        }
-        if (map.get(0) == map.get(1) && map.get(2) == map.get(3)) {
-            System.out.println("studnia");
-        } else if (isNorth(map)) {
-            map.put(0, map.get(0) - map.get(1));
-            if (isWest(map)) {
-                map.put(2, map.get(2) - map.get(3));
-            } else {
-                map.put(3, map.get(3) - map.get(2));
+
+            if (map.get(0) == map.get(1) && map.get(2) == map.get(3)) {
+                System.out.println("studnia");
+            } else if (isNorth(map)) {
+                shortStepsSouthNorth(map,0,1);
+                map.remove(1);
+                shortStepEq(map,2,3);
+                show(map);
+            } else if (!isNorth(map)) {
+                shortStepsSouthNorth(map,1,0);
+                map.remove(0);
+                shortStepEq(map,2,3);
+                show(map);
+
             }
-        } else if (!isNorth(map)) {
-            map.put(1, map.get(1) - map.get(0));
-            if (isWest(map)) {
-                map.put(2, map.get(2) - map.get(3));
-            } else {
-                map.put(3, map.get(3) - map.get(2));
-            }
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() != 0) {
-                System.out.println(entry.getKey() + " " + entry.getValue());
-            }
+
         }
     }
 
@@ -65,5 +59,26 @@ public class SKARBI {
             return true;
         }
         return false;
+    }
+    private static void show(Map<Integer, Integer> map) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+        }
+    }
+    private static Map<Integer,Integer> shortStepsSouthNorth(Map<Integer,Integer> map, int directionOne, int directionTwo) {
+
+        map.put(directionOne, map.get(directionOne) - map.get(directionTwo));
+        map.remove(directionTwo);
+        return map;
+    }
+    private static Map<Integer,Integer> shortStepEq(Map<Integer,Integer> map, int directionOne, int directionTwo) {
+        if (isWest(map)) {
+            shortStepsSouthNorth(map,directionOne,directionTwo);
+        } else {
+            shortStepsSouthNorth(map,directionTwo,directionOne);
+        }
+        return map;
     }
 }
